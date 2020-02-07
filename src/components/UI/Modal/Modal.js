d50169a0877c898b00369c4a6c7c08d360702030
luis  torralbo <luis.torralbo20@gmail.com>
2020-02-06 23:10:54 -0300

    improving performane changing modal behavior

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import BackDrop from '../BackDrop/BackDrop';
 
-const Modal = props => (
-  <Aux>
-    <BackDrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className="Modal"
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show != this.props.show;
+  }
+
+  componentDidUpdate() {
+    console.log('updated');
+  }
+
+  render() {
+    return (
+      <Aux>
+        <BackDrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className="Modal"
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
 export default Modal;
